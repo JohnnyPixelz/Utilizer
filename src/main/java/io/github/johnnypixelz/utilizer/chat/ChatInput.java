bf083360b874f117c88df6ad19ca6ebcba1aed5d
JohnnyPixelz <kolovosjohn@yahoo.com>
2021-04-28 17:05:19 +0300

    Updated ChatInput with no backwards compatibility, removed ScoreboardWrapper

--- src/main/java/io/github/johnnypixelz/utilizer/chat/ChatInput.java
+++ src/main/java/io/github/johnnypixelz/utilizer/chat/ChatInput.java
@@ -1,5 +1,7 @@
 package io.github.johnnypixelz.utilizer.chat;
 
+import io.github.johnnypixelz.utilizer.Scheduler;
+import io.github.johnnypixelz.utilizer.provider.Provider;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,7 +10,6 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.function.Consumer;
 
@@ -17,46 +18,43 @@ public class ChatInput implements Listener {
     /**
      * The player that ChatInput will listen to
      */
-    private Player player;
-
-    /**
-     * The plugin's instance
-     */
-    private JavaPlugin instance;
+    private final Player player;
 
     /**
      * The consumer that will be called once a player sends a message
      */
-    private Consumer<AsyncPlayerChatEvent> onChatEvent;
+    private Consumer<String> messageAsync;
 
     /**
      * The consumer that will be called once a player sends a message but is also synchronized
      */
-    private Consumer<AsyncPlayerChatEvent> onChatEventSynchronized;
+    private Consumer<String> messageSync;
 
     /**
-     * @param plugin the main plugin instance
      * @param player the player that ChatInput will listen to
      */
-    public ChatInput(JavaPlugin plugin, Player player) {
+    private ChatInput(Player player) {
         this.player = player;
-        this.instance = plugin;
 
-        Bukkit.getPluginManager().registerEvents(this, plugin);
+        Bukkit.getPluginManager().registerEvents(this, Provider.getPlugin());
     }
 
-    /**
-     * Set the consumer that should be called whenever the player sends a message
-     */
-    public void setOnChatEvent(Consumer<AsyncPlayerChatEvent> onChatEvent) {
-        this.onChatEvent = onChatEvent;
+    public static void sync(Player player, Consumer<String> message) {
+        ChatInput chatInput = new ChatInput(player);
+        chatInput.setMessageSync(message);
     }
 
-    /**
-     * Set the consumer that should be called whenever the player sends a message but is also synchronized
-     */
-    public void setOnChatEventSynchronized(Consumer<AsyncPlayerChatEvent> onChatEventSynchronized) {
-        this.onChatEventSynchronized = onChatEventSynchronized;
+    public static void async(Player player, Consumer<String> message) {
+        ChatInput chatInput = new ChatInput(player);
+        chatInput.setMessageAsync(message);
+    }
+
+    public void setMessageAsync(Consumer<String> messageAsync) {
+        this.messageAsync = messageAsync;
+    }
+
+    public void setMessageSync(Consumer<String> messageSync) {
+        this.messageSync = messageSync;
     }
 
     /**
@@ -68,13 +66,13 @@ public class ChatInput implements Listener {
     private void onAsyncChatEvent(AsyncPlayerChatEvent event) {
         if (!player.getUniqueId().equals(event.getPlayer().getUniqueId())) return;
 
-        if (onChatEvent != null) {
-            onChatEvent.accept(event);
+        if (messageAsync != null) {
+            messageAsync.accept(event.getMessage());
             event.setCancelled(true);
         }
 
-        if (onChatEventSynchronized != null) {
-            Bukkit.getScheduler().runTask(instance, () -> onChatEventSynchronized.accept(event));
+        if (messageSync != null) {
+            Scheduler.sync(() -> messageSync.accept(event.getMessage()));
             event.setCancelled(true);
         }
 
