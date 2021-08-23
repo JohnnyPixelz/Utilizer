dde44fed4a9dd10cf86af77e97b702e049fe7b10
JohnnyPixelz <kolovosjohn@yahoo.com>
2021-08-23 20:05:13 +0300

    Added File methods to Persistence

--- src/main/java/io/github/johnnypixelz/utilizer/file/Persistence.java
+++ src/main/java/io/github/johnnypixelz/utilizer/file/Persistence.java
@@ -3,37 +3,57 @@ package io.github.johnnypixelz.utilizer.file;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import io.github.johnnypixelz.utilizer.plugin.Provider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
+import java.lang.reflect.Type;
 
 public class Persistence {
     private static Gson gson = new Gson();
 
-    public static <T> T loadFile(String fileName) {
-        File file = new File(Provider.getPlugin().getDataFolder().getPath() + File.separator + fileName + ".json");
+    public static <T> T loadFile(@NotNull File file) {
+        return loadFile(file, null);
+    }
 
+    public static <T> T loadFile(@NotNull File file, @Nullable TypeToken<T> token) {
         try {
-            return gson.fromJson(new FileReader(file), new TypeToken<T>(){}.getType());
+            Type type = token != null
+                    ? token.getType()
+                    : new TypeToken<T>(){}.getType();
+            return gson.fromJson(new FileReader(file), type);
         } catch (FileNotFoundException ignored) {
             return null;
         }
     }
 
-    public static <T> T loadFile(String fileName, TypeToken<T> token) {
+    public static <T> T loadFile(@NotNull String fileName) {
         File file = new File(Provider.getPlugin().getDataFolder().getPath() + File.separator + fileName + ".json");
-
-        try {
-            return gson.fromJson(new FileReader(file), token.getType());
-        } catch (FileNotFoundException ignored) {
-            return null;
-        }
+        return loadFile(file);
     }
 
-    public static boolean saveFile(String fileName, Object object) {
+    public static <T> T loadFile(@NotNull String fileName, @Nullable TypeToken<T> token) {
+        File file = new File(Provider.getPlugin().getDataFolder().getPath() + File.separator + fileName + ".json");
+        return loadFile(file, token);
+    }
+
+    public static boolean saveFile(@NotNull File file, @NotNull Object object) {
+        return saveFile(file, object, null);
+    }
+
+    public static boolean saveFile(@NotNull String fileName, @NotNull Object object) {
         Provider.getPlugin().getDataFolder().mkdirs();
+        File file = new File(Provider.getPlugin().getDataFolder().getPath() + File.separator + fileName + ".json");
+        return saveFile(file, object);
+    }
+
+    public static <T> boolean saveFile(@NotNull File file, @NotNull Object object, @Nullable TypeToken<T> token) {
         try {
-            FileWriter writer = new FileWriter(Provider.getPlugin().getDataFolder().getPath() + File.separator + fileName + ".json");
-            gson.toJson(object, writer);
+            FileWriter writer = new FileWriter(file);
+            Type type = token != null
+                    ? token.getType()
+                    : new TypeToken<T>(){}.getType();
+            gson.toJson(object, type, writer);
             writer.close();
         } catch (FileNotFoundException ignored) {
         } catch (IOException ex) {
@@ -43,18 +63,10 @@ public class Persistence {
         return true;
     }
 
-    public static <T> boolean saveFile(String fileName, Object object, TypeToken<T> token) {
+    public static <T> boolean saveFile(@NotNull String fileName, @NotNull Object object, @Nullable TypeToken<T> token) {
         Provider.getPlugin().getDataFolder().mkdirs();
-        try {
-            FileWriter writer = new FileWriter(Provider.getPlugin().getDataFolder().getPath() + File.separator + fileName + ".json");
-            gson.toJson(object, token.getType(), writer);
-            writer.close();
-        } catch (FileNotFoundException ignored) {
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return false;
-        }
-        return true;
+        File file = new File(Provider.getPlugin().getDataFolder().getPath() + File.separator + fileName + ".json");
+        return saveFile(file, object, token);
     }
 
     public static void setGson(Gson gson) {
