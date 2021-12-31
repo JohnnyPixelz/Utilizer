7e10f493aac19555d4e060b3660db23e1f256fda
JohnnyPixelz <kolovosjohn@yahoo.com>
2021-12-31 17:46:40 +0200

    Implemented messaging utilities

---
+++ src/main/java/io/github/johnnypixelz/utilizer/config/Message.java
@@ -0,0 +1,153 @@
+package io.github.johnnypixelz.utilizer.config;
+
+import com.cryptomorin.xseries.messages.ActionBar;
+import com.cryptomorin.xseries.messages.Titles;
+import io.github.johnnypixelz.utilizer.text.Colors;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Message {
+    private String message;
+    private List<String> messageList;
+    private Sound sound;
+    private String title;
+    private String subtitle;
+    private String actionbar;
+
+    @NotNull
+    public Message send(@NotNull Player player) {
+        if (message != null) {
+            player.sendMessage(Colors.color(message));
+        }
+
+        if (messageList != null) {
+            for (String line : messageList) {
+                player.sendMessage(Colors.color(line));
+            }
+        }
+
+        if (sound != null) {
+            player.playSound(player.getLocation(), sound, 1, 1);
+        }
+
+        if (title != null) {
+            if (subtitle != null) {
+                Titles.sendTitle(player, Colors.color(title), Colors.color(subtitle));
+            } else {
+                Titles.sendTitle(player, Colors.color(title), null);
+            }
+        }
+
+        if (actionbar != null) {
+            ActionBar.sendActionBar(player, Colors.color(actionbar));
+        }
+
+        return this;
+    }
+
+    @NotNull
+    public Message send(@NotNull List<Player> players) {
+        for (Player player : players) {
+            send(player);
+        }
+
+        return this;
+    }
+
+    @NotNull
+    public Message map(@NotNull String target, @NotNull String replacement) {
+        return map(line -> line.replace(target, replacement));
+    }
+
+    @NotNull
+    public Message map(@NotNull Function<String, String> mapper) {
+        if (message != null) {
+            message = mapper.apply(message);
+        }
+
+        if (messageList != null) {
+            for (int index = 0; index < messageList.size(); index++) {
+                messageList.set(index, mapper.apply(messageList.get(index)));
+            }
+        }
+
+        if (title != null) {
+            if (subtitle != null) {
+                subtitle = mapper.apply(subtitle);
+            }
+
+            title = mapper.apply(title);
+        }
+
+        if (actionbar != null) {
+            actionbar = mapper.apply(actionbar);
+        }
+
+        return this;
+    }
+
+    @Nullable
+    public String getMessage() {
+        return message;
+    }
+
+    public Message setMessage(@Nullable String message) {
+        this.message = message;
+        return this;
+    }
+
+    @Nullable
+    public List<String> getMessageList() {
+        return messageList;
+    }
+
+    public Message setMessageList(@Nullable List<String> messageList) {
+        this.messageList = messageList;
+        return this;
+    }
+
+    @Nullable
+    public Sound getSound() {
+        return sound;
+    }
+
+    public Message setSound(@Nullable Sound sound) {
+        this.sound = sound;
+        return this;
+    }
+
+    @Nullable
+    public String getTitle() {
+        return title;
+    }
+
+    public Message setTitle(@Nullable String title) {
+        this.title = title;
+        return this;
+    }
+
+    @Nullable
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public Message setSubtitle(@Nullable String subtitle) {
+        this.subtitle = subtitle;
+        return this;
+    }
+
+    @Nullable
+    public String getActionbar() {
+        return actionbar;
+    }
+
+    public Message setActionbar(@Nullable String actionbar) {
+        this.actionbar = actionbar;
+        return this;
+    }
+}
---
+++ src/main/java/io/github/johnnypixelz/utilizer/config/Messages.java
@@ -0,0 +1,117 @@
+package io.github.johnnypixelz.utilizer.config;
+
+import com.cryptomorin.xseries.XSound;
+import io.github.johnnypixelz.utilizer.text.Colors;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.function.Function;
+
+public class Messages {
+
+    public static void send(Player player, String message) {
+        player.sendMessage(Colors.color(message));
+    }
+
+    public static void send(Player player, List<String> message) {
+        for (String line : message) {
+            send(player, line);
+        }
+    }
+
+    public static void send(Player player, String... message) {
+        for (String line : message) {
+            send(player, line);
+        }
+    }
+
+    public static void send(List<Player> players, String message) {
+        for (Player player : players) {
+            send(player, message);
+        }
+    }
+
+    public static void send(List<Player> players, List<String> message) {
+        for (Player player : players) {
+            send(player, message);
+        }
+    }
+
+    public static void send(List<Player> players, String... message) {
+        for (Player player : players) {
+            send(player, message);
+        }
+    }
+
+    public static void send(Player player, String message, Function<String, String> mapper) {
+        send(player, mapper.apply(message));
+    }
+
+    public static void send(Player player, List<String> messages, Function<String, String> mapper) {
+        for (String line : messages) {
+            send(player, mapper.apply(line));
+        }
+    }
+
+    public static void send(List<Player> players, String message, Function<String, String> mapper) {
+        send(players, mapper.apply(message));
+    }
+
+    public static void send(List<Player> players, List<String> messages, Function<String, String> mapper) {
+        for (String line : messages) {
+            send(players, mapper.apply(line));
+        }
+    }
+
+    @NotNull
+    public Message cfg(@NotNull String config, @NotNull String configPath) {
+        FileConfiguration fileConfiguration = Configs.get(config);
+
+        Message message = new Message();
+
+        if (fileConfiguration.isConfigurationSection(configPath)) {
+            ConfigurationSection section = fileConfiguration.getConfigurationSection(configPath);
+            for (String key : section.getKeys(false)) {
+                switch (key.toLowerCase()) {
+                    case "message":
+                        if (section.isList(key)) {
+                            message.setMessageList(section.getStringList(key));
+                            break;
+                        }
+                        if (section.isString(key)) {
+                            message.setMessage(section.getString(key));
+                            break;
+                        }
+                        break;
+                    case "title":
+                        if (!section.isString(key)) break;
+                        message.setTitle(section.getString(key));
+                        break;
+                    case "subtitle":
+                        if (!section.isString(key)) break;
+                        message.setSubtitle(section.getString(key));
+                        break;
+                    case "actionbar":
+                        if (!section.isString(key)) break;
+                        message.setActionbar(section.getString(key));
+                        break;
+                    case "sound":
+                        if (!section.isString(key)) break;
+                        String soundString = section.getString(key);
+                        XSound.matchXSound(soundString).ifPresent(xSound -> message.setSound(xSound.parseSound()));
+                        break;
+                }
+            }
+        } else if (fileConfiguration.isList(configPath)) {
+            message.setMessageList(fileConfiguration.getStringList(configPath));
+        } else if (fileConfiguration.isString(configPath)) {
+            message.setMessage(fileConfiguration.getString(configPath));
+        }
+
+        return message;
+    }
+
+}
