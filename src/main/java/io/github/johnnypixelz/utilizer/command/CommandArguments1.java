5fb63fc923c64bfacafe50913315092ac21490f5
JohnnyPixelz <kolovosjohn@yahoo.com>
2024-02-23 18:25:58 +0200

    Deleted command package from master branch

--- src/main/java/io/github/johnnypixelz/utilizer/command/CommandArguments1.java
+++
@@ -1,7 +0,0 @@
-package io.github.johnnypixelz.utilizer.command;
-
-public interface CommandArguments1<A1> {
-
-    void execute(A1 a1);
-
-}
--- src/main/java/io/github/johnnypixelz/utilizer/command/Commands.java
+++
@@ -1,9 +0,0 @@
-package io.github.johnnypixelz.utilizer.command;
-
-public class Commands {
-
-    public static CommandBuilder builder(String root) {
-        return new CommandBuilder(root);
-    }
-
-}
