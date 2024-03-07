7df2930d8fa45bb0bbcf8f578cd050d9aa34dc67
JohnnyPixelz <kolovosjohn@yahoo.com>
2024-03-07 03:58:10 +0200

    Implemented command tab completion
    Took 30 minutes

--- src/main/java/io/github/johnnypixelz/utilizer/command/BukkitCommand.java
+++ src/main/java/io/github/johnnypixelz/utilizer/command/BukkitCommand.java
@@ -2,14 +2,12 @@ package io.github.johnnypixelz.utilizer.command;
 
 import io.github.johnnypixelz.utilizer.plugin.Logs;
 import io.github.johnnypixelz.utilizer.plugin.Provider;
-import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginIdentifiableCommand;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.*;
 
 public class BukkitCommand extends org.bukkit.command.Command implements PluginIdentifiableCommand {
     private final Command command;
@@ -42,7 +40,35 @@ public class BukkitCommand extends org.bukkit.command.Command implements PluginI
     @NotNull
     @Override
     public List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
-        return super.tabComplete(sender, alias, args);
+        Logs.info("Tab Complete");
+        Logs.info("Alias: " + alias);
+        Logs.info("Args: " + String.join(", ", args));
+
+        Command currentCommand = command;
+
+        final Queue<String> arguments = new LinkedList<>(List.of(args));
+        while (!arguments.isEmpty()) {
+            final String argument = arguments.poll();
+
+            if (arguments.isEmpty()) {
+                return currentCommand.getSubcommands()
+                        .stream()
+                        .map(Command::getLabels)
+                        .flatMap(List::stream)
+                        .filter(subcommand -> subcommand.startsWith(argument))
+                        .toList();
+            }
+
+            final Optional<Command> firstSubcommandFound = currentCommand.getSubcommands()
+                    .stream()
+                    .filter(subcommand -> subcommand.getLabels().contains(argument))
+                    .findFirst();
+            if (firstSubcommandFound.isEmpty()) return Collections.emptyList();
+
+            currentCommand = firstSubcommandFound.get();
+        }
+
+        return Collections.emptyList();
     }
 
     @NotNull
