4359999adb9c1f89f11d7dedf572881635771cc8
JohnnyPixelz <kolovosjohn@yahoo.com>
2024-05-08 21:51:12 +0300

    Made ConfigInventory public
    Took 42 seconds

--- src/main/java/io/github/johnnypixelz/utilizer/inventory/inventories/ConfigInventory.java
+++ src/main/java/io/github/johnnypixelz/utilizer/inventory/inventories/ConfigInventory.java
@@ -1,31 +1,15 @@
 package io.github.johnnypixelz.utilizer.inventory.inventories;
 
-import io.github.johnnypixelz.utilizer.config.Configs;
 import io.github.johnnypixelz.utilizer.inventory.CustomInventory;
 import org.bukkit.configuration.ConfigurationSection;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Optional;
 
 public class ConfigInventory extends CustomInventory {
 
-    public static Optional<ConfigInventory> from(@Nullable String configFile, @Nullable String configPath) {
-        if (configFile == null || configPath == null) return Optional.empty();
-
-        final ConfigurationSection configurationSection = Configs.get(configFile).getConfigurationSection(configPath);
-        if (configurationSection == null) return Optional.empty();
-
-        return Optional.of(new ConfigInventory(configurationSection));
-    }
-
-    public static ConfigInventory from(@Nonnull ConfigurationSection section) {
-        return new ConfigInventory(section);
-    }
-
     private final ConfigurationSection section;
 
-    private ConfigInventory(ConfigurationSection section) {
+    public ConfigInventory(@Nonnull ConfigurationSection section) {
         this.section = section;
     }
 
